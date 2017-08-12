38ac18d8437fe41d621b01e8effc853afd64072f
vbeakovic <valter.beakovic@gmail.com>
2017-08-12 11:17:53 +0200

    Note object extended with a getter and a static function

--- models/Note.js
+++ models/Note.js
@@ -7,4 +7,14 @@ module.exports = class Note {
     this.title = title;
     this.body = body;
   }
+  get JSON() {
+    return JSON.stringify({
+      key: this.key, title: this.title, body: this.body
+    });
+  }
+  static fromJSON(json) {
+    var data = JSON.parse(json);
+    var note = new Note(data.key, data.title, data.body);
+    return note;
+  }
 };
