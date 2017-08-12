768116446bf99a69993dd11fa3cd69fd6ab8ebf1
vbeakovic <valter.beakovic@gmail.com>
2017-08-12 10:46:13 +0200

    logging configured

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var FileStreamRotator = require('file-stream-rotator');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -9,15 +10,34 @@ var index = require('./routes/index');
 // var users = require('./routes/users');
 var notes = require('./routes/notes');
 
+var error = require('debug')('notes:error');
+
+process.on('uncaughtException', function(err) {
+  error("I've crashed!!! - " + (err.stack || err));
+})
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+var accessLogStream;
+if (process.env.REQUEST_LOG_FILE) {
+  var logDirectory = path.dirname(process.env.REQUEST_LOG_FILE);
+  fs.existsSync(logDirectory || fs.mkdirSync(logDirectory));
+  accessLogStream = FileStreamRotator.getStream({
+    filename: process.env.REQUEST_LOG_FILE,
+    frequency: 'daily',
+    verbose: false
+  });
+}
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev'));
+app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev', {
+  stream: accessLogStream ? accessLogStream : process.stdout
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -35,15 +55,25 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    error((err.status || 500) + ' ' + error.message);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-  // render the error page
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  error((err.status || 500) + ' ' + error.message);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
